afc61883f16d8dd480f5ff4ecb9e803440e57f93
therealalexolthoff <alex@1452design.com>
2024-06-19 22:45:35 -0400

    Finished repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let newString = ''
+    if (num < 0){
+        return "ERROR"
+    }
+    for (let counter = 0; counter < num; counter++){
+        newString += string
+    }
+    return newString
 };
 
 // Do not edit below this line
