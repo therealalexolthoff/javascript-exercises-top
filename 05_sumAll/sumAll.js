88acbc51464dce42025605db4b74a932d236c6d9
therealalexolthoff <alex@1452design.com>
2024-07-14 18:50:24 -0400

    Finish sumall with all test cases passing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    const isStartNumber = typeof(start) === "number"
+    const isEndNumber = typeof(end) === "number"
+    const isStartPositive = start > 0
+    const isEndPositive = end > 0
 
+    let finalNum = 0
+    if (!isStartNumber || !isEndNumber || !isStartPositive || !isEndPositive){
+        return "ERROR"
+    }
+    if (start < end){
+        for (let i = start; i < end+1; i++){
+            finalNum += i;
+        }
+    }
+    else if (start > end){
+        for (let i = end; i < start+1; i++){
+            finalNum += i;
+        }
+    }
+    
+    return finalNum
 };
 
 // Do not edit below this line
