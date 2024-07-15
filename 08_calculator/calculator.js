89052e4d8fbc456dc04e19ec60db45bbb56ae2a8
therealalexolthoff <alex@1452design.com>
2024-07-15 07:13:43 -0400

    Finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  const numbers = args.map((num) => parseInt(num))
+  console.log(numbers)
+  let num = numbers.reduce((num,counter) => num + counter, 0)
+  return num
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((counter,num) => counter * num)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorialNum = 1
+  if (a === 0 || a === 1){
+    return 1
+  }
+  for (let i = a; i > 0; i--){
+    factorialNum *= i;
+    }
+  return factorialNum
 };
 
 // Do not edit below this line
