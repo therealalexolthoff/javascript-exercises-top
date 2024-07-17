07daf5fe9eb1d369b265a4af4614bfa2e1312bed
therealalexolthoff <alex@1452design.com>
2024-07-16 21:16:45 -0400

    1 test passing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(people) {
+    let oldestPerson = {age: 0, person: null}
+    people.forEach((person)=> {
+        console.log(person)
+        if (person.yearOfDeath){
+            const age = person.yearOfDeath - person.yearOfBirth
+            if (age > oldestPerson.age){
+                oldestPerson.age = age
+                oldestPerson.person = person
+            }
+        }
+}   )
+return oldestPerson.person
+}
 // Do not edit below this line
 module.exports = findTheOldest;
