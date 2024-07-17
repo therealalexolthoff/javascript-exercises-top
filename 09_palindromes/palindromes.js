107b8ee0a219a54b8d8f9ec0e75839a1a9e247fc
therealalexolthoff <alex@1452design.com>
2024-07-17 13:23:17 -0400

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (testString) {
+   testString = testString.toLowerCase()
+   let stringArray = testString.split("").filter((element) => element !== " ").filter((element)=> element !== ".").filter(element=> element !== ",").filter(element => element !== "!")
 
+   let fixedTestString = stringArray.join("")
+   let newString = stringArray.reverse().join("")
+   return fixedTestString === newString
 };
-
 // Do not edit below this line
 module.exports = palindromes;
