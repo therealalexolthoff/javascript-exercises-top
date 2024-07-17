8194b8b01feedbf36c42da4fae114fbc414d762f
therealalexolthoff <alex@1452design.com>
2024-07-17 13:01:15 -0400

    Finish fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(point) {
+    if (point * -1 > 0){
+        return "OOPS"
+    }
+    if (typeof point === "string"){
+       point = parseInt(point)
+    }
+    let counter = 0
+    let fibonacciNumber1 = 0
+    let fibonacciNumber2 = 1
+    let sum = 0
+    if (point === 1){
+        return 1
+    }
+    while (counter < point-1){
+        sum = fibonacciNumber1 + fibonacciNumber2
+        fibonacciNumber1 = fibonacciNumber2
+        fibonacciNumber2 = sum
+        counter += 1
+    }
+    return sum
 };
 
 // Do not edit below this line
